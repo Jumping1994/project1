987a337b5d5835bf4fbba1e3bd438e8b86b5946a
Ubuntu <Jumping@databases.kp52g2xot4yexlzklc5jgdyecb.bx.internal.cloudapp.net>
2016-03-29 05:07:28 +0000

    third version

--- webserver/server.py
+++ webserver/server.py
@@ -9,17 +9,16 @@ Go to http://localhost:8111 in your browser
 A debugger such as "pdb" may be helpful for debugging.
 Read about it online.
 """
+import random
 import datetime
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask,url_for, request, render_template, g, redirect, Response
+from flask import Flask,url_for,session, request, render_template, g, redirect, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-#
 # The following uses the sqlite3 database test.db -- you can use this for debugging purposes
 # However for the project you will need to connect to your Part 2 database in order to use the
 # data
@@ -135,11 +134,13 @@ def login():
   error=None
   if request.method=='POST':
     name=request.form['name']
+    ID=request.form['ID']
     if name not in name_ID:
       error='name does not exist';
-    elif int(request.form['ID'])!=int(name_ID[name]):
+    elif int(ID)!=int(name_ID[name]):
       error='Invalid ID,please try again.'
     else:
+      session['userID']=ID
       return redirect(url_for('showRestaurant'))
   return render_template('login.html',error=error)
 
@@ -152,31 +153,72 @@ def showRestaurant():
     restaurants = cursor.fetchall()
     cursor2=g.conn.execute("SELECT name,R.restaurant_ID,table_no,size from restaurant R, tables_owns T where R.restaurant_ID=T.restaurant_ID")
     cursor3=g.conn.execute("SELECT table_no,restaurant_id from reserves")
-
+    c2=cursor2.fetchall()
+    c3=cursor3.fetchall()
     name={}
     error=None
     if request.method=='POST':
-     restaurant_name=request.form['restaurant name']
-     size=request.form['table size']
+      restaurant_name=request.form['restaurant name']
+      size=request.form['table size']
 
-     for res2 in cursor2:
-       name[res2[0]]=0
-     if restaurant_name not in name:
-       error="This restaurant is not available"
-       return render_template("showRestaurant.html", restaurants = restaurants,error=error)
-     if error==None:
-       for res2 in cursor2:
-         if res2[0]==restaurant_name and size<res2[3]:
-           reserveation_exist=False
-           for res3 in cursor3:
-             if res3[0]==res2[2] and res3[1]==res2[1]:
-               reserveation_exist=True;
-               break;
-             if reserveation_exist==False:
-               g.conn.execute("INSERT into reserves (table_no,restaurant_id,time) VALUES (res2[2],res2[1],datetime.date.today())")
-               return render_template("showRestaurant.html", restaurants = restaurants,error=error,table_no=res2[2])
-    # error="We can't find a table available for you"	
-   # return render_template("showRestaurant.html", restaurants = restaurants,error=error)
+      for res2 in c2:
+        name[res2[0]]=0
+      if restaurant_name not in name:
+        error="This restaurant is not available"
+        return render_template("showRestaurant.html", restaurants = restaurants,error=error)
+      error="xx"
+      for res2 in c2:
+        if res2[0]==restaurant_name and int(size)<res2[3]:
+          reserveation_exist=False
+          for res3 in c3:
+            if res3[0]==res2[2] and res3[1]==res2[1]:
+              reserveation_exist=True
+              break
+          if reserveation_exist==False:
+            ID=session.get('userID')
+            temp=(ID,res2[2],res2[1],datetime.date.today())
+            session['restaurantID']=res2[1]
+            g.conn.execute("INSERT into reserves (customer_id,table_no,restaurant_id,time) VALUES (%s,%s,%s,%s)",temp)
+            return redirect(url_for('placeorder'))
+     # error="We can't find a table available for you"	
+    return render_template("showRestaurant.html", restaurants = restaurants,error=error)
+
+@app.route('/placeorder', methods=['GET', 'POST'])
+def placeorder():
+	cursor = g.conn.execute("select name,description,prices from dish")
+	dishes = cursor.fetchall()
+	dish={}
+	error=None
+	for res in dishes:
+		dish[res[0]]=res[2]
+	cursor.close()
+	price = 0
+	dishnames={}
+	finals=[]
+       # render_template('placeorder.html', error=error, dishes=dishes)
+	if request.method == 'POST':
+		dishnames[request.form['dish_name1']]= request.form['quantity1']
+		dishnames[request.form['dish_name2']]= request.form['quantity2']
+		dishnames[request.form['dish_name3']]= request.form['quantity3']
+		for dishname in dishnames:
+			if dishname not in dish:
+				error = 'Invalid dish';
+				return render_template('placeorder.html', error=error, dishes=dishes)
+			else:
+				#finals.append(dishnames[dishname])
+				price += float(dishnames[dishname]) * float(dish[dishname])
+		order_no = 11111
+		globalcustomerid=session.get('userID')
+		globalresid = session.get('restaurantID')
+		tmp = (order_no,globalcustomerid,globalresid)
+		g.conn.execute("INSERT into order_places (order_no, customer_id,restaurant_id) values (%s,%s,%s)",tmp)
+		for names in dishnames:
+			tmp1 = (order_no,names)
+			g.conn.execute("INSERT into contains (order_no,name) values (%s,%s)",tmp1)
+		return redirect(url_for('payment'))
+        return render_template('placeorder.html', error=error, dishes=dishes)
+
+
   # Flask uses Jinja templates, which is an extension to HTML where you can
   # pass data to a template and dynamically generate HTML based on the data
   # (you can think of it as simple PHP)
@@ -186,7 +228,7 @@ def showRestaurant():
   #
   # context are the variables that are passed to the template.
   # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
+  # accessible as a variable in index.html:i
   #
   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
@@ -194,6 +236,7 @@ def showRestaurant():
   #     # creates a <div> tag for each element in data
   #     # will print: 
   #     #
+
   #     #   <div>grace hopper</div>
   #     #   <div>alan turing</div>
   #     #   <div>ada lovelace</div>
@@ -245,6 +288,7 @@ if __name__ == "__main__":
 
     HOST, PORT = host, port
     print "running on %s:%d" % (HOST, PORT)
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(host=HOST, port=PORT, debug=true, threaded=threaded)
 
 
